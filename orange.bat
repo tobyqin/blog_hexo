99ab1e987664bfc161eeb8d34107e3da8a7d66c9
Toby Qin <toby.qin@live.com>
2016-10-01 14:19:42 +0800

    update and save blog.

---
+++ orange.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo Start to generate blog...
+git add -A
+git commit -m "update and save blog."
+git push origin master
+
+echo Done!
+echo.
+echo.
+echo To preview the blog, use "hexo s"
+echo To deploy the blog, use "hexo d"
+echo.
+echo.
