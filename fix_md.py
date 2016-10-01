27ac0d160b66fc49d6726243b2b19d4ce5815a44
Toby Qin <toby.qin@live.com>
2016-10-01 10:07:32 +0800

    rename posts

--- fix_md.py
+++ fix_md.py
@@ -1,31 +1,38 @@
 import re
 import tempfile
 import os
-
-
-def fix_line(line):
-    return line.replace('\n', '<br/>')
+import glob
 
 
 def fix_file(file_name):
-    new_file = os.path.join(tempfile.gettempdir(), 'new.md')
-    is_code = False
-    with open(new_file, 'w') as f1:
-        with open(file_name) as f:
-            for line in f:
-                if line.startswith('```'):
-                    is_code = not is_code
-                    f1.write(line)
-                    continue
+    names = os.path.split(file_name)
+    folder = names[0]
+    name = names[-1]
+    new_name = name
+    time_prefix = None
+    with open(file_name) as f:
+        for line in f:
+            if line.startswith('date:'):
+                time_prefix = line.split()[1]
+                break
 
-                if is_code:
-                    f1.write(fix_line(line))
-                else:
-                    f1.write(line)
+    if time_prefix is None:
+        raise Exception('Cannot find time stamp for {}'.format(file_name))
+    else:
+        time_prefix += "-"
 
-    print new_file
+    if name.startswith('20'):
+        new_name = time_prefix + name[11:]
+    else:
+        new_name = time_prefix + name
+
+    new_name = os.path.join(folder, new_name)
+    print "{}=>{}".format(file_name, new_name)
+    os.rename(file_name, new_name)
 
 
 if __name__ == '__main__':
-    test_file = r"C:\Users\toby.qin\src\hexo\source\_posts\from-csharp-to-python-overview.md"
-    fix_file(test_file)
+    current_path = os.path.dirname(__file__)
+    post_path = os.path.join(current_path, 'source', '_posts')
+    for f in glob.glob(os.path.join(post_path, "*.md")):
+        fix_file(f)
