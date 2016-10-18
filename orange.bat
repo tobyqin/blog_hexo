40e69b15ebd3763b6c3437cd7749e93b2634dbac
Toby Qin <toby.qin@ef.com>
2016-10-18 09:40:42 +0800

    update blog

--- orange.bat
+++ orange.bat
@@ -8,6 +8,11 @@ echo Pull and merge...
 git pull origin master
 git push origin master
 
+echo Backup to Git.OSC
+git remote add osc git@git.oschina.net:ggqq/hexo.git
+git pull osc master
+git push osc master
+
 echo Well done!
 echo.
 echo.
