5819c2f1d9e10d89bc500cdd86f8b536c62ef353
Toby Qin <toby.qin@live.com>
2017-03-24 07:47:55 +0800

    update blog.

--- @push.py
+++ @push.py
@@ -13,5 +13,5 @@ os.system('git push osc master')
 
 # backup to coding.net
 os.system('git remote add coding git@git.coding.net:tobyqin/blog.git')
-os.system('git pull osc master')
-os.system('git push osc master')
+os.system('git pull coding master')
+os.system('git push coding master')
