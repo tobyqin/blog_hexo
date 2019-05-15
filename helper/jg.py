7909760f3a03bbe1027339d012673f12409230bc
Toby Qin <toby.qin@live.com>
2019-05-15 21:49:30 +0800

    update website.

--- helper/jg.py
+++ helper/jg.py
@@ -10,31 +10,37 @@ from os.path import join, dirname
 
 from webdavfs.webdavfs import WebDAVFS
 
-if 'jg_login' not in os.environ and 'jg_password' not in os.environ:
-    raise EnvironmentError('Please set "jg_login" and "jg_password" in environment!')
-
 target_dir = join(dirname(dirname(__file__)), '_mobile')
-url = 'https://dav.jianguoyun.com/dav'
-options = {
-    'url': url,
-    'login': os.environ['jg_login'],
-    'password': os.environ['jg_password'],
-    'root': '/Blog/New'
-}
 
-fs = WebDAVFS(**options)
+
+def get_options():
+    if 'jg_login' not in os.environ and 'jg_password' not in os.environ:
+        raise EnvironmentError('Please set "jg_login" and "jg_password" in environment!')
+
+    return {
+        'url': 'https://dav.jianguoyun.com/dav',
+        'login': os.environ['jg_login'],
+        'password': os.environ['jg_password'],
+        'root': '/Blog/New'
+    }
+
+
+def get_fs():
+    options = get_options()
+    return WebDAVFS(**options)
 
 
 def urlparse(string):
     return urllib.parse.quote(string)
 
 
-def get_new_files():
+def get_new_files(fs):
     files = fs.listdir('.')[1:]
     return [f for f in files if not (f.startswith('!') or f.startswith('！'))]
 
 
 def download_file(remote_file):
+    options = get_options()
     options['remote'] = urlparse(remote_file)
     options['local'] = join(target_dir, remote_file)
     cmd = "curl -u '{login}:{password}' '{url}{root}/{remote}' -o '{local}'".format_map(options)
@@ -42,13 +48,15 @@ def download_file(remote_file):
 
 
 def delete_file(remote_file):
+    options = get_options()
     options['remote'] = urlparse(remote_file)
     cmd = "curl -X DELETE -u '{login}:{password}' '{url}{root}/{remote}'".format_map(options)
     os.system(cmd)
 
 
 def run():
-    for f in get_new_files():
+    fs = get_fs()
+    for f in get_new_files(fs):
         download_file(f)
         delete_file(f)
 
