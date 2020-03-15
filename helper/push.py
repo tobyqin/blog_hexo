0479d5748266259c5ac51c3f285e8aeeee21dbca
Toby Qin <toby.qin@live.com>
2020-03-15 11:17:18 +0800

    update website.

--- helper/push.py
+++ helper/push.py
@@ -15,14 +15,14 @@ def run():
     os.system('git push origin master')
 
     # backup to git osc
-    os.system('git remote add osc git@git.oschina.net:ggqq/hexo.git')
-    os.system('git pull osc master')
-    os.system('git push osc master')
+    # os.system('git remote add osc git@git.oschina.net:ggqq/hexo.git')
+    # os.system('git pull osc master')
+    # os.system('git push osc master')
 
     # backup to coding.net
-    os.system('git remote add coding git@git.coding.net:tobyqin/blog.git')
-    os.system('git pull coding master')
-    os.system('git push coding master')
+    # os.system('git remote add coding git@git.coding.net:tobyqin/blog.git')
+    # os.system('git pull coding master')
+    # os.system('git push coding master')
 
 
 if __name__ == '__main__':
