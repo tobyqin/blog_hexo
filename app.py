866587a9f5dc1177927aeeb82bda1e0cd716b4b1
Toby Qin <toby.qin@live.com>
2020-04-20 22:03:06 +0800

    build app to serve blog

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+import werkzeug
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='public')
+
+root = 'public'
+
+
+@app.route('/')
+def hello_world():
+    return send_from_directory(root, 'index.html')
+
+
+@app.route('/<path:filename>')
+def index(filename: str):
+    if filename.endswith('/'):
+        filename += 'index.html'
+    try:
+        return flask.send_from_directory(root, filename)
+    except werkzeug.exceptions.NotFound as e:
+        if filename.endswith("/"):
+            return flask.send_from_directory(root, filename + "index.html")
+        raise e
+
+
+app.run()
