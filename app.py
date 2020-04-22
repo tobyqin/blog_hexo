0bf1e4eba49615e176569c41859c70f8f766e5c4
Toby Qin <toby.qin@live.com>
2020-04-22 22:52:23 +0800

    improve app

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-import flask
 import werkzeug
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='public')
 
 root = 'public'
+default_page = 'index.html'
 
 
 @app.route('/')
 def hello_world():
-    return send_from_directory(root, 'index.html')
+    return send_from_directory(root, default_page)
 
 
 @app.route('/<path:filename>')
 def index(filename: str):
     if filename.endswith('/'):
-        filename += 'index.html'
+        filename += default_page
     try:
-        return flask.send_from_directory(root, filename)
+        return send_from_directory(root, filename)
     except werkzeug.exceptions.NotFound as e:
         if filename.endswith("/"):
-            return flask.send_from_directory(root, filename + "index.html")
+            return send_from_directory(root, filename + default_page)
         raise e
 
 
